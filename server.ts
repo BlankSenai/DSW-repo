9ef8e3fc608e4679a4998ca02f5fa4cfe8740de4
BlankSenai <gabriel_blank@estudante.sesisenai.org.br>
2026-08-13 00:34:20 +0000

    feat: implementa crud de tarefas

--- server.ts
+++ server.ts
@@ -1,8 +1,23 @@
 import express from "express"
+import { randomUUID, type UUID } from "node:crypto"
 
 const app = express()
 
 const port = 3000
+
+// middleware para ler body json
+app.use(express.json())
+
+interface Tarefa {
+    id: UUID,
+    title: String,
+    status: "Pendente" | "Finalizado" | "Em andamento"
+}
+
+// banco provisorio em RAM
+let temp_bd: Tarefa[] = [
+    {id: randomUUID(), title: "Estudar REST", status: "Pendente"}
+]
 
 // GET /health - Integridade do sistema
 app.get('/api/health', (_, res) => {
@@ -19,6 +34,52 @@ app.get('/api/version', (_, res) => {
         version: "1.0.0"
     })
 })
+
+// GET /tasks - Busca tarefas cadastradas
+app.get('/api/tasks', (_, res) => {
+    res.json(
+        temp_bd
+    )
+})
+
+// POST /tasks - Cria uma nova tarefa
+app.post('/api/tasks', (req, res) => {
+    const { title } = req.body
+
+    const newTask: Tarefa = {
+        id: randomUUID(),
+        title: title,
+        status: "Pendente"
+    }
+
+    temp_bd.push(newTask)
+
+    res.status(201).json({
+        newTask
+    })
+})
+
+// DELETE /tasks - Deleta uma tarefa
+app.delete('/api/tasks/:id', (req, res) => {
+    const deleteId = req.params.id
+
+    const task: Tarefa | undefined = temp_bd.find(t => t.id == deleteId)
+
+    if (task) {
+        temp_bd = temp_bd.filter(t => t.id != deleteId)
+    
+        res.json({
+            message: "Tarefa excluída com sucesso"
+        })
+    } else {
+        res.status(404).json({
+            message: "Tarefa não encontrada"
+        })
+    }
+
+})
+
+
 
 app.listen(port, () => {
     console.log(`Servidor funfando em http://localhost:${port}`)
